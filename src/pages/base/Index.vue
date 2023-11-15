79d3aebfb3484418e3203aeef7a88b0b007c9052
chelsea.chang <yuan097@kimo.com>
2023-11-15 17:10:11 +0800

    [feat] import FormRadio from vue2-components

--- src/pages/base/Index.vue
+++ src/pages/base/Index.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="base">base page.</div>
+  <div class="base">
+    <FormRadio v-model="radioValue" value="1" @change="radioChange">
+      1
+    </FormRadio>
+    <FormRadio v-model="radioValue" value="2" @change="radioChange">
+      2
+    </FormRadio>
+  </div>
 </template>
 
 <script>
+import { FormRadio } from "vue2-components";
 export default {
   name: "PageBase",
+  components: {
+    FormRadio,
+  },
+  data() {
+    return {
+      radioValue: "",
+    };
+  },
+  methods: {
+    radioChange(value) {
+      console.log("radioChange", value);
+    },
+  },
 };
 </script>
 
